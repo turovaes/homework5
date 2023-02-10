57f3a69674cbc4176b7074feef1892799469dad9
turovaes <turovaes@gmail.com>
2023-02-10 19:51:24 +0100

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] GetRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int FindSum (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int[] newArray = GetRandomArray(6, 10, 50);
+PrintArray(newArray); 
+int answer = FindSum(newArray);
+ Console.WriteLine($"Сумму чисел  {answer}");
